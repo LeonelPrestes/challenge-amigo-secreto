3c4cef42f7bf21f8a63faa64fcdecd7f17c9bfc9
LeonelPrestes <leonelprestes90@gmail.com>
2025-09-22 20:34:03 -0300

    adicionado a lista de nomes, a função adicionar Amigo e a função mostrar lista

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo(){
+    let input = document.getElementById('amigo');
+    if (input.value == ""){
+        alert('Por favor, insira o nome de um amigo(a) para adicionar à lista.');
+        input.focus()
+        return
+    }
+    if (amigos.includes(input.value)) {
+        alert(`Amigo(a) ${input.value} já está na lista.`);
+        input.value = '';
+        input.focus();
+        return
+    }
+    else {
+        amigos.push(input.value);
+        input.value = '';
+        mostrarLista()
+        input.focus()
+    }
+    
+}
+
+function mostrarLista(){
+    let lista = document.getElementById('listaAmigos')
+    lista.innerHTML = ""
+    amigos.forEach(element => { 
+        lista.innerHTML += `<li>${element}</li>`
+    });
+    console.log(amigos);
+}
